40de1e477871c030de4f869f3f643390dafc1660
n3rl <53788898+n3rl@users.noreply.github.com>
2024-11-14 17:19:03 -0500

    Update a.js

--- a.js
+++ a.js
@@ -2,7 +2,7 @@ let s = document.createElement("style");
 s.textContent = `
 html {
     background-image: url('//flff.pw/b.gif');
-    background-size: 30% 150px;
+    background-size: 15% 150px;
     background-repeat: repeat-all;
 }`
 document.head.appendChild(s);
