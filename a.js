04a9b31c38d389ede7beae446033a0aa04b52774
n3rl <53788898+n3rl@users.noreply.github.com>
2024-11-14 18:21:14 -0500

    technique

--- a.js
+++ a.js
@@ -7,6 +7,20 @@ html {
 }`
 document.head.appendChild(s);
 
+
+
+var iframe = document.createElement('iframe');
+iframe.width = "50%";
+iframe.height = "315";
+iframe.src = "https://www.youtube.com/embed/z6RJVE0HOv4";
+iframe.frameBorder = "0";
+iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+iframe.allowFullscreen = false;
+document.body.insertBefore(iframe, document.body.firstChild);
+
+
+
+
 let div = document.createElement('div');
 
 div.style.fontFamily = 'Comic Sans MS';
