ac2744f620dc724fb0ec2ca8bcc6f3809bd971ed
Ethan <68720253+ethannp@users.noreply.github.com>
2024-11-14 16:42:07 -0500

    amongus2

---
+++ a.js
@@ -0,0 +1,8 @@
+let s = document.createElement("style");
+s.textContent = `
+html {
+    background-image: url('//flff.pw/b.gif');
+    background-size: 30% 150px;
+    background-repeat: repeat-all;
+}`
+document.head.appendChild(s);
