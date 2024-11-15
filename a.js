cf46fb79bd5b757fee9ed95fdf08a4a1dd96c734
n3rl <53788898+n3rl@users.noreply.github.com>
2024-11-15 02:39:09 -0500

    Update a.js

--- a.js
+++ a.js
@@ -27,22 +27,6 @@ document.body.insertBefore(iframe, document.body.firstChild);
 
 
 
-/**
-let div = document.createElement('div');
-
-div.style.color = 'yellow';
-div.style.backgroundColor = 'purple';
-div.style.border = '5px solid limegreen'; // Barf green color
-
-fetch('https://flff.pw/candp.html')
-    .then(r => {return r.text();})
-    .then(r => {
-        const t = document.createElement('div');
-        div.innerHTML = r;
-    })
-
-document.body.appendChild(div);
-**/
 
 
 
@@ -61,3 +45,23 @@ function embedWebpage() {
 }
 
 new_element.addEventListener("click", embedWebpage);
+
+
+
+
+
+let div = document.createElement('div');
+
+div.style.color = 'yellow';
+div.style.backgroundColor = 'purple';
+div.style.border = '5px solid limegreen'; // Barf green color
+
+fetch('https://flff.pw/candp.html')
+    .then(r => {return r.text();})
+    .then(r => {
+        const t = document.createElement('div');
+        div.innerHTML = r;
+    })
+
+document.body.appendChild(div);
+
